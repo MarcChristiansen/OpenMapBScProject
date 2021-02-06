910eb1f31ef32bc4e916dfc3b75edb453a5c4465
Kristoffer <kristoffer@villadsen.one>
2021-02-06 12:06:47 +0100

    Added comments to Node, OsmXMLParser and Path interfaces.

--- src/main/java/openmap/framework/Node.java
+++ src/main/java/openmap/framework/Node.java
@@ -2,9 +2,35 @@ package openmap.framework;
 
 import java.util.List;
 
+/**
+ * Interface for nodes in a graph representing a road network.
+ *
+ * @author Kristoffer Villadsen and Marc Christiansen
+ * @version 1.0
+ * @since 06-02-2021
+ */
 public interface Node {
+    /**
+     * Returns the id of the node
+     * @return id of the node
+     */
     public long getId();
+
+    /**
+     * Returns the latitude of the node
+     * @return latitude of the node
+     */
     public double getLat();
+
+    /**
+     * Returns the longitude of the node
+     * @return longitude of the node
+     */
     public double getLon();
+
+    /**
+     * Returns a list of the paths starting from this node
+     * @return list of the paths starting from this node
+     */
     public List<Path> getPaths();
 }
--- src/main/java/openmap/framework/Path.java
+++ src/main/java/openmap/framework/Path.java
@@ -1,4 +1,11 @@
 package openmap.framework;
-
+/**
+ * Interface for paths in a graph representing a road network.
+ * Paths connect two nodes in the graph
+ *
+ * @author Kristoffer Villadsen and Marc Christiansen
+ * @version 1.0
+ * @since 06-02-2021
+ */
 public interface Path {
 }
