7c00873023b5bc2666caccca92967015fb68b948
Kristoffer <kristoffer@villadsen.one>
2021-02-09 16:00:02 +0100

    Added documentation to some of the interfaces

--- src/main/java/openmap/framework/Graph.java
+++ src/main/java/openmap/framework/Graph.java
@@ -2,6 +2,12 @@ package openmap.framework;
 
 import java.util.Map;
 
+/**
+ * Interface that represents a graph, contains a map of all the nodes, and the bounds of the graph
+ * @author Kristoffer Villadsen and Marc Christiansen
+ * @version 1.0
+ * @since 09-02-2021
+ */
 public interface Graph {
 
     public Map<Long, Node> getNodeMap();
--- src/main/java/openmap/framework/graphBuilder.java
+++ src/main/java/openmap/framework/graphBuilder.java
@@ -3,7 +3,17 @@ package openmap.framework;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Interface of objects used to build graphs
+ * @author Kristoffer Villadsen and Marc Christiansen
+ * @version 1.0
+ * @since 09-02-2021
+ */
 public interface graphBuilder {
 
+    /**
+     * Create a graph and return it
+     * @return a graph
+     */
     public Graph createGraph();
 }
