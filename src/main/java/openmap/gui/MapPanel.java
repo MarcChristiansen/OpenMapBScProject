38b1f39cc76dd231ab50cff811a2e1b17ac28cd2
Marc Christiansen <mc@marcs.dk>
2021-02-16 17:32:32 +0100

    Gui semi working

--- src/main/java/openmap/gui/MapPanel.java
+++ src/main/java/openmap/gui/MapPanel.java
@@ -8,42 +8,92 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.Arrays;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
 import java.util.Map;
-import java.util.Random;
 
 class MapPanel extends JPanel {
 
-    private int nodeRadius = 5;
+    private final int nodeRadius = 5; //Node radius at default zoom or below
+    private double panSpeed = 1;
+    private double zoomFactor = 1;
     private Graph graph;
+    private double panX;
+    private double panY;
+
+    public int getScaledNodeRadius() {
+
+        if(zoomFactor >= 1) {
+            return nodeRadius;
+        }
+        return (int)(nodeRadius/zoomFactor);
+    }
 
     public MapPanel(Graph graph) {
         this.graph = graph;
         setBorder(BorderFactory.createLineBorder(Color.black));
 
-        /*addMouseListener(new MouseAdapter() {
-            public void mousePressed(MouseEvent e) {
-                moveSquare(e.getX(),e.getY());
-            }
-        });
+        //Set initial graphics location
+        panX = -(graph.getBounds().getMaxX()-graph.getBounds().getMinX());
+        panY = -(graph.getBounds().getMaxY()-graph.getBounds().getMinY());
 
-        addMouseMotionListener(new MouseAdapter() {
-            public void mouseDragged(MouseEvent e) {
-                moveSquare(e.getX(),e.getY());
+        MouseAdapter ma = new MouseAdapter() {
+            private Point origPoint;
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                origPoint = new Point(e.getPoint());
             }
-        });*/
+
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (origPoint != null) {
+                    int deltaX = origPoint.x - e.getX();
+                    int deltaY = origPoint.y - e.getY();
+
+                    panX += deltaX*panSpeed/getZoomFactor();
+                    panY += deltaY*panSpeed/getZoomFactor();
+                    origPoint = e.getPoint();
+                    repaint();
+                    }
+                }
+
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                //Zoom in
+                if(e.getWheelRotation()<0){
+                    setZoomFactor(1.1*getZoomFactor());
+                    repaint();
+                }
+                //Zoom out
+                if(e.getWheelRotation()>0){
+                    setZoomFactor(getZoomFactor()/1.1);
+                    repaint();
+                }
+
+            }
+        };
+
+        this.addMouseListener(ma);
+        this.addMouseMotionListener(ma);
+        addMouseWheelListener(ma);
 
     }
 
-   /* private void moveSquare(int x, int y) {
-        int OFFSET = 1;
-        if ((squareX!=x) || (squareY!=y)) {
-            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
-            squareX=x;
-            squareY=y;
-            repaint(squareX,squareY,squareW+OFFSET,squareH+OFFSET);
+    public double getZoomFactor() {
+        return zoomFactor;
+    }
+
+    private void setZoomFactor(double factor){
+        if(factor<this.zoomFactor){
+            this.zoomFactor=this.zoomFactor/1.1;
         }
-    }*/
+        else{
+            this.zoomFactor=factor;
+        }
+    }
 
     /**
      * Draw a correctly centered circle
@@ -52,7 +102,7 @@ class MapPanel extends JPanel {
      * @param y y coordinate
      * @param radius The radius to draw
      */
-    public static void drawCircle(Graphics g, int x, int y, int radius) {
+    private static void drawCircle(Graphics g, int x, int y, int radius) {
 
         int diameter = radius * 2;
 
@@ -67,35 +117,77 @@ class MapPanel extends JPanel {
     }
 
     @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
+    protected void paintComponent(Graphics gg) {
+        this.setBorder(null);
+        super.paintComponent(gg);
+        Graphics2D g = (Graphics2D) gg;
+        AffineTransform matrix = g.getTransform(); // Backup
+
+
+
+
+
+        //Zoom related stuff and Panning stuff
+        AffineTransform at = new AffineTransform();
+        at.scale(zoomFactor, zoomFactor);
+
+        at.translate(-panX, -panY);
+
+        g.transform(at);
+        g.scale(1,-1);
+        g.translate(0,getHeight());
+
+
+        //Rotation stuff
+
         g.setColor(Color.RED);
 
-        Random rand = new Random();
+        int tempCounter = 0;
+
+        drawCircle(g,10 , 10, getScaledNodeRadius());
 
         for (Map.Entry<Long, Node> entry : graph.getNodeMap().entrySet())
         {
+            tempCounter += 1;
+
+            if(tempCounter == 1000){
+
             Node node = entry.getValue();
 
             g.setColor(Color.RED);
-            int x = (int) ((node.getX() + graph.getBounds().getMaxX()));
-            int y = (int) ((node.getY() + graph.getBounds().getMaxY()));
+            int x = (int) ((node.getX() - graph.getBounds().getMinX()));
+            int y = (int) ((node.getY() - graph.getBounds().getMinY()));
             //System.out.println(graph.getBounds().getMinX());
             //System.out.println(graph.getBounds().getMinY());
             //System.out.println("(x: " + x + ", y: " + y + ")" + "(lat: " + node.getLat() + ", lon: " + node.getLon() + ")");
-            drawCircle(g,x , y, nodeRadius);
+            drawCircle(g,x , y, getScaledNodeRadius());
+
+            /*g.drawLine((int)(0),
+                    (int)(0),
+                    (int)(x),
+                    (int)(y));*/
+
+            //System.out.println(x + " " + y);
 
             g.setColor(Color.BLACK);
+            tempCounter = 0;
+            }
 
+
+
+
+            /*
             for (Path p : node.getPaths()) {
                 g.drawLine((int)(node.getX() - graph.getBounds().getMinX()),
                             (int)(node.getY() - graph.getBounds().getMinY()),
                             (int)(p.getDestination().getX() - graph.getBounds().getMinX()),
                             (int)(p.getDestination().getY() - graph.getBounds().getMinY()));
-            }
+            }*/
         }
 
-        System.out.println("");
+        System.out.println("(panX: " + panX + ", " + "panY" + panY + ")");
+
+        g.setTransform(matrix); // Restore
 
     }
 }
