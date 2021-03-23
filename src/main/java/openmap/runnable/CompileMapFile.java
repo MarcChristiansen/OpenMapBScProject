3510c7c85bc82a73d4e629c230095a4554ecfc99
Marc Christiansen <mc@marcs.dk>
2021-03-23 13:02:10 +0100

    Added auto detection of raw map file type when compiling map and removed older implementations

--- src/main/java/openmap/runnable/CompileMapFile.java
+++ src/main/java/openmap/runnable/CompileMapFile.java
@@ -2,9 +2,11 @@ package openmap.runnable;
 
 import openmap.framework.Graph;
 import openmap.framework.OsmParser;
+import openmap.parsing.OsmXmlParserImpl;
 import openmap.parsing.ParsingUtil;
 import openmap.standard.GraphBuilderImpl;
 import openmap.parsing.OsmiumPbfParserImpl;
+import org.openstreetmap.osmosis.osmbinary.file.FileFormatException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -17,7 +19,7 @@ import static openmap.parsing.json.DiskUtility.createJsonGraphJGen;
 /**
  * Simple class to help create xml files from snippets we use
  */
-public class CompilePbf {
+public class CompileMapFile {
     public static void main(String[] args) throws IOException {
         String path = "";
         String outPath = "";
@@ -34,9 +36,22 @@ public class CompilePbf {
                     "Enter ser path : ");
         }
 
+        OsmParser parser = null;
 
+        String extension = getFileExtension(path);
 
-        OsmParser parser = new OsmiumPbfParserImpl(path, ParsingUtil.getMinimizedAllowedValues());
+        System.out.println(extension);
+
+        if(extension.equals("osm")){
+            parser = new OsmXmlParserImpl(path);
+        }
+        else if(extension.equals("pbf")){
+            parser = new OsmiumPbfParserImpl(path, ParsingUtil.getMinimizedAllowedValues());
+        }
+        else{
+            throw new FileFormatException("File needs to be either pbf or osm format. a " + extension + " file was given.");
+        }
+
         openmap.framework.graphBuilder graphBuilder = new GraphBuilderImpl(parser);
         ((GraphBuilderImpl)graphBuilder).setShouldOptimizeGraph(false);
 
@@ -47,6 +62,15 @@ public class CompilePbf {
         createJsonGraphJGen(graph, outPath);
         System.out.println("Serialized graph data is saved in " + outPath);
 
+    }
+
+    private static String getFileExtension(String path) {
+        String extension = "";
+        int i = path.lastIndexOf('.');
+        if (i > 0) {
+            extension = path.substring(i+1);
+        }
+        return extension;
     }
 
     private static String readLine(String format, Object... args) throws IOException {
